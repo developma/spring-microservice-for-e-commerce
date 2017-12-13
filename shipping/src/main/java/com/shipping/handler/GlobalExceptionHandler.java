c38c1154078194cdd535c88d312c9de602d79da9
developma <developer.daisuke.maeno@gmail.com>
2017-12-13 15:39:33 +0900

    Remove unnecessary package name.

--- shipping/src/main/java/com/shipping/handler/GlobalExceptionHandler.java
+++ shipping/src/main/java/com/shipping/handler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.shipping.handler;
 
-import com.common.domain.ErrorInformation;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
