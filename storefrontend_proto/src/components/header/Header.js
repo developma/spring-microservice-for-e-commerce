2419be081b54c28cc01349f9a0d29ed99886bd01
developma <developer.daisuke.maeno@gmail.com>
2018-01-23 14:09:19 +0900

    Add ordering logic.

--- storefrontend_proto/src/components/header/Header.js
+++ storefrontend_proto/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import request from 'superagent'
 
 export default class Header extends Component {
   constructor (props) {
@@ -17,10 +18,63 @@ export default class Header extends Component {
     this.itemList.forEach(item => {
       totalFee = totalFee + item.price
     })
-
     this.setState({
       totalUnit: this.itemList.length,
       totalFee: totalFee
+    })
+  }
+
+  onCheckOutClick (e) {
+    if (this.state.totalUnit === 0) {
+      return alert('please add some items to cart.')
+    }
+
+    const orderedItem = []
+    this.itemList.forEach(item => {
+      orderedItem.push({
+        registerId: 0,
+        id: item.id,
+        unit: 1,
+        versionno: 0
+      })
+    })
+
+    // This is dummy data for passing process of order.
+    const order = {
+      registerId: 0,
+      orderedItem: orderedItem,
+      senderName: 'TestTestTest',
+      address: {
+        registerId: 0,
+        zipCode: '123-4567',
+        location: 'TestLocTestLoc',
+        receiverName: 'NameNameName'
+      }
+    }
+
+    request.post('http://localhost:8210/shipping/order/')
+    .set('Content-Type', 'application/json')
+    .send(order)
+    .end((err, res) => {
+      if (err || !res.ok) {
+        const errorInfo = JSON.parse(res.text)
+        if (errorInfo) {
+          window.alert(errorInfo.message)
+        } else {
+          window.alert('post error' + JSON.stringify(err))
+          console.log(JSON.stringify(err))
+        }
+      } else if (res.text === 'success') {
+        window.alert('your order has been completed.')
+        console.log(JSON.stringify(res))
+        this.itemList.splice(0, this.itemList.length)
+        this.setState({
+          totalUnit: 0,
+          totalFee: 0
+        })
+      } else {
+        window.alert('an error occurs during ordering process.')
+      }
     })
   }
 
@@ -33,7 +87,7 @@ export default class Header extends Component {
           </div>
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <button className="btn btn-success navbar-btn">
+              <button className="btn btn-success navbar-btn" onClick={e => this.onCheckOutClick(e)}>
                 Checkout <span className="badge">{this.state.totalUnit}({this.state.totalFee} Yen)</span>
               </button>
             </li>
